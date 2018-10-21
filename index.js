28a667b3d6a41f428273053d3546a08c0f6b1e8f
lavina <luvina1009@gmail.com>
2018-10-21 20:08:13 +0530

    removed mongo

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/db";
+// var MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb://localhost:27017/db";
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 app.use(bodyParser.json());
@@ -10,24 +10,24 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 var conn;
-MongoClient.connect(url,{ useNewUrlParser: true } , function(err, db) {
-  if (err) throw err;
-  else 
-  console.log("Connected on port "+PORT); 
-  this.conn = db; 
-});
+// MongoClient.connect(url,{ useNewUrlParser: true } , function(err, db) {
+//   if (err) throw err;
+//   else 
+//   console.log("Connected on port "+PORT); 
+//   this.conn = db; 
+// });
 
 app.get('/',(req,res)=>{
     res.json('Connected successfully');
 })
 
-app.get('/data', function (req, res) {
-  var dbo = this.conn.db("db");
-  //Find all documents in the customers collection:
-  dbo.collection("test").find({}).toArray(function(err, result) {
-    if (err) throw err;
-    console.log(result);
-    res.json(result);
-  });
-})
+// app.get('/data', function (req, res) {
+//   var dbo = this.conn.db("db");
+//   //Find all documents in the customers collection:
+//   dbo.collection("test").find({}).toArray(function(err, result) {
+//     if (err) throw err;
+//     console.log(result);
+//     res.json(result);
+//   });
+// })
 
