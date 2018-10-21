1f07b47948a7b2b3e42601c5c46a74cf7977d794
lavina <luvina1009@gmail.com>
2018-10-22 00:49:08 +0530

    post api

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+const rp = require('request-promise');
+const $ = require('cheerio');
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://heroku_92150sf3:c3bghg1817nkncmtbsafjpblje@ds237713.mlab.com:37713/heroku_92150sf3";
@@ -32,3 +34,44 @@ app.get('/',(req,res)=>{
 
 app.get('/data', getAllResponses);
 
+app.post('/', function (req, res) {
+    // console.log(req.body);
+    var intent = req.body.req1;
+    console.log(intent);
+    var dbo = this.conn.db("heroku_92150sf3");
+    // Find all documents in the customers collection:
+    console.log(intent);
+    dbo.collection("test").find({
+      intent: intent
+    }).toArray(function (err, result) {
+      if (err) throw err;
+      console.log(result);
+      if (result.length > 0) {
+        console.log(result);
+        console.log("Found");
+        res.json("Found");
+      } else {
+        console.log("Not Found");
+        var url = "https://www.google.co.in/search?q=" + intent + "&oq=" + intent;
+        console.log(url);
+        rp(url)
+          .then(function (html) {
+            console.log($('div > div > div > div > span', html).text());
+            const info = $('div > div > div > div > span', html).text();
+            const obj = {
+              intent: intent,
+              info: info
+            };
+            dbo.collection("test").insertOne(obj, function (err, res) {
+              if (err) throw err;
+              
+            });
+            res.json("Inserted");
+          })
+          .catch(function (err) {});
+      }
+    });
+  
+  
+  
+  })
