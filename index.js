26deca5b3b599bdd449be642fa12bb7dd57a3bc0
lavina <luvina1009@gmail.com>
2018-10-21 19:25:02 +0530

    first commit

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var app = express();
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/db";
-
-app.listen(8100);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
@@ -13,11 +13,11 @@ var conn;
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   else 
-  console.log("Connected"); 
+  console.log("Connected on port "+PORT); 
   this.conn = db; 
 });
 
-app.post('/data', function (req, res) {
+app.get('/data', function (req, res) {
   var dbo = this.conn.db("db");
   //Find all documents in the customers collection:
   dbo.collection("test").find({}).toArray(function(err, result) {
