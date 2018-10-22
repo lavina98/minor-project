b20abd01a3bbf4dffad5afb602a6153620ad7510
lavina <luvina1009@gmail.com>
2018-10-22 12:52:32 +0530

    post api changed

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 var conn;
-MongoClient.connect(url,{ useNewUrlParser: true } , function(err, db) {
+MongoClient.connect(url,{ useNewUrlParser: true } ,function(err, db) {
   if (err) throw err;
   else 
   console.log("Connected on port "+PORT); 
@@ -35,44 +35,108 @@ app.get('/',(req,res)=>{
 app.get('/data', getAllResponses);
 
 app.post('/', function (req, res) {
-    // console.log(req.body);
-    console.log(request);
-    var intent = req.body.req1;
-    console.log(intent);
-    var dbo = this.conn.db("heroku_92150sf3");
-    // Find all documents in the customers collection:
-    console.log(intent);
-    dbo.collection("test").find({
-      intent: intent
-    }).toArray(function (err, result) {
-      if (err) throw err;
+  console.log(req.body);
+  // var intent = req.body.req1;
+  var queryVal=req.body.queryResult.parameters.Formulae;
+  console.log("query value is"+queryVal);
+  var dbo = this.conn.db("heroku_92150sf3");
+  var ans='';
+  // Find all documents in the customers collection:
+  dbo.collection("test").find({
+    topic:queryVal
+  }).toArray(function (err, result) {
+    if (err) throw err;
+    if (result.length > 0) {
       console.log(result);
-      if (result.length > 0) {
-        console.log(result);
-        console.log("Found");
-        res.json("Found");
-      } else {
-        console.log("Not Found");
-        var url = "https://www.google.co.in/search?q=" + intent + "&oq=" + intent;
-        console.log(url);
-        rp(url)
-          .then(function (html) {
-            console.log($('div > div > div > div > span', html).text());
-            const info = $('div > div > div > div > span', html).text();
-            const obj = {
-              intent: intent,
-              info: info
-            };
-            dbo.collection("test").insertOne(obj, function (err, res) {
-              if (err) throw err;
-              
-            });
-            res.json("Inserted");
-          })
-          .catch(function (err) {});
-      }
+      console.log("Found");
+      ans=result[0].info;
+      console.log('result is :'+ans);
+      var obj = {
+        fulfillmentText: ans,
+        fulfillmentMessages: [
+          {
+            card: {
+              title: "card title",
+              subtitle: "card text",
+              imageUri: "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+              buttons: [
+                {
+                  text: "button text",
+                  postback: "https://assistant.google.com/"
+                }
+              ]
+            }
+          }
+        ],
+        source: "example.com",
+        payload: {
+          google: {
+            expectUserResponse: true,
+            richResponse: {
+              items: [
+                {
+                  simpleResponse: {
+                    textToSpeech: "this is a simple response"
+                  }
+                }
+              ]
+            }
+          }
+        }
+      }; 
+      res.json(obj);
+    } else {
+      console.log("Not Found");
+      var url = "https://www.google.co.in/search?q=" + queryVal + "&oq=" + queryVal;
+      rp(url)
+        .then(function (html) {
+          console.log($('div > div > div > div > span', html).text());
+          const info = $('div > div > div > div > span', html).text();
+          const obj = {
+            topic:queryVal ,
+            info: info
+          };
+          ans=info;
+          console.log('ans is  '+ans);
+          dbo.collection("test").insertOne(obj, function (err, res) {
+            if (err) throw err;
+
+          });
+          var obj1 = {
+            fulfillmentText: ans,
+            fulfillmentMessages: [
+              {
+                card: {
+                  title: "card title",
+                  subtitle: "card text",
+                  imageUri: "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+                  buttons: [
+                    {
+                      text: "button text",
+                      postback: "https://assistant.google.com/"
+                    }
+                  ]
+                }
+              }
+            ],
+            source: "example.com",
+            payload: {
+              google: {
+                expectUserResponse: true,
+                richResponse: {
+                  items: [
+                    {
+                      simpleResponse: {
+                        textToSpeech: "this is a simple response"
+                      }
+                    }
+                  ]
+                }
+              }
+            }
+          }; 
+          res.json(obj1);
+        }).catch(function (err) {});
+     }
+    })    
     });
-  
-  
-  
-  })
